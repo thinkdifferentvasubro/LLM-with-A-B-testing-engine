21031a294dd8cdd78dc8007e674c3b001a50513b
thinkdifferentvasubro <thinkdifferentvasubro@gmail.com>
2026-08-23 18:48:21 +0530

    ab test rag commit

--- src/stats_/ab_test_selector.py
+++ src/stats_/ab_test_selector.py
@@ -9,7 +9,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from RAG.rag_pipeline import VectorDBManager
+from src.RAG.rag_pipeline import VectorDBManager
 
 
 class ABTestSelector:
